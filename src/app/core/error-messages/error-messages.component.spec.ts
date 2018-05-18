a1cf7b4386a664e9e68289b5d3d492442ca32b43
csvinhal <cristiano.vinhal10@gmail.com>
2018-05-17 21:06:35 -0300

    error-messages test

--- src/app/core/error-messages/error-messages.component.spec.ts
+++ src/app/core/error-messages/error-messages.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormControl } from '@angular/forms';
-import { By } from '@angular/platform-browser';
+import { FormControl, Validators } from '@angular/forms';
 
 import { ErrorMessagesComponent } from './error-messages.component';
 
@@ -18,7 +17,22 @@ describe('ErrorMessagesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorMessagesComponent);
     component = fixture.componentInstance;
-    component.control = new FormControl(null);
+
+    component.control = new FormControl(null, Validators.compose([
+      Validators.required,
+      Validators.min(1),
+      Validators.max(10)
+    ]));
+
+    component.errors = {
+      'required': 'O campo é obrigatório.',
+      'min': 'O valor deve ser maior que 0.',
+      'max': 'O valor deve ser menor que 100.'
+    };
+
+    fixture.componentInstance.messages = fixture.componentInstance.errorMessage;
+
+    component.control.markAsTouched();
     fixture.detectChanges();
   });
 
@@ -27,7 +41,53 @@ describe('ErrorMessagesComponent', () => {
   });
 
   it('should show a error', () => {
-    component.control.setErrors({ 'required': 'O campo é obrigatório.' });
-    expect(fixture.debugElement.nativeElement.id).toBeTruthy();
+    expect(fixture.nativeElement.querySelectorAll('.form-control-feedback').length).toBe(1);
+  });
+
+  it('should show two errors', () => {
+    component.control.setErrors([
+      { required: true },
+      { min: true }
+    ]);
+
+    fixture.detectChanges();
+    fixture.whenRenderingDone().then(() => {
+      expect(fixture.nativeElement.querySelectorAll('.form-control-feedback').length).toBe(2);
+    });
+  });
+
+  it('should show three errors', () => {
+    component.control.setErrors([
+      { required: true },
+      { min: true },
+      { max: true }
+    ]);
+
+    fixture.detectChanges();
+    fixture.whenRenderingDone().then(() => {
+      expect(fixture.nativeElement.querySelectorAll('.form-control-feedback').length).toBe(3);
+    });
+  });
+
+  it('should show required error', () => {
+    expect(fixture.nativeElement.querySelectorAll('#error-0')[0].innerText).toBe('O campo é obrigatório.');
+  });
+
+  it('should show min error', () => {
+    component.control.setValue(-1);
+
+    fixture.detectChanges();
+    fixture.whenRenderingDone().then(() => {
+      expect(fixture.nativeElement.querySelectorAll('#error-0')[0].innerText).toBe('O valor deve ser maior que 0.');
+    });
+  });
+
+  it('should show max error', () => {
+    component.control.setValue(101);
+
+    fixture.detectChanges();
+    fixture.whenRenderingDone().then(() => {
+      expect(fixture.nativeElement.querySelectorAll('#error-0')[0].innerText).toBe('O valor deve ser menor que 100.');
+    });
   });
 });
